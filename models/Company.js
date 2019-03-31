92b953060a83353ea97bd183fec874c6e61df178
andrew_koper <andrew.koper@gmail.com>
2019-03-31 15:56:33 -0400

    Created company Mongoose model

---
+++ models/Company.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var CompanySchema = new mongoose.Schema({
+    name: String,
+    industry: String,
+    phone: String,
+    street: String,
+    city: String,
+    state: String,
+    zip: String,
+    updated_date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
