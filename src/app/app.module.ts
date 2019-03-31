8799365b115baf821814c735dc7ac5d392a824cb
andrew_koper <andrew.koper@gmail.com>
2019-03-31 16:05:37 -0400

    Added 'create companies' imports, declarations and providers to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { BookCreateComponent } from './book-create/book-create.component';
-import { BookEditComponent } from './book-edit/book-edit.component';
-import { CompanyCreateComponent } from './company/company-create/company-create.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { PersonCreateComponent } from './person/person-create/person-create.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BookComponent} from './book/book.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
+import {BookCreateComponent} from './book-create/book-create.component';
+import {BookEditComponent} from './book-edit/book-edit.component';
+import {CompanyCreateComponent} from './company/company-create/company-create.component';
+import {HomepageComponent} from './homepage/homepage.component';
+import {PersonCreateComponent} from './person/person-create/person-create.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {CompanyService} from './company.service';
+import {HttpErrorHandler} from './http-error-handler.service';
+import {CompanyComponent} from './company/company/company.component';
+import {CompaniesComponent} from './company/companies/companies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BookComponent,
-    BookDetailComponent,
-    BookCreateComponent,
-    BookEditComponent,
-    CompanyCreateComponent,
-    HomepageComponent,
-    PersonCreateComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BookComponent,
+        BookDetailComponent,
+        BookCreateComponent,
+        BookEditComponent,
+        CompanyCreateComponent,
+        HomepageComponent,
+        PersonCreateComponent,
+        CompanyComponent,
+        CompaniesComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    providers: [
+        CompanyService,
+        HttpErrorHandler,
+        {provide: LocationStrategy, useClass: HashLocationStrategy}],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
